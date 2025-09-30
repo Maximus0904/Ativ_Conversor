625c53e5248327080eb5e4b8987f774d2b7efa86
Maximus0904 <marcio.santo.junior@escola.pr.gov.br>
2025-09-30 17:50:57 -0300

    Create conversor_de_dec_para_bin.py

---
+++ conversor_de_dec_para_bin.py
@@ -0,0 +1,12 @@
+print("Conversor de sistemas")
+num = int(input("Digite o Número Decimal = "))
+resultado = ""
+if num == 0:
+    resultado = "0"
+else:
+    num_D = num
+    while num_D > 0:
+        resto = num_D % 2
+        num_D = num_D // 2
+        resultado = str(resto) + resultado
+print(f"O valor de '{num}'em binário corresponde a: {resultado}")
